89c9c5d7864f38e826b4637e26ff847e3adca74c
Mahajanchahat20 <mahajanchahat@gmail.com>
2025-04-12 21:24:46 +0530

    dark mode applied to register page

--- client/src/Pages/Register.js
+++ client/src/Pages/Register.js
@@ -1,14 +1,22 @@
 import RegisterForm from '../Components/RegisterForm/RegisterForm'
 import {Outlet,Link} from 'react-router-dom'
 //This will be shown in all pages
-export default function Register()
+export default function Register({darkMode})
 {
+    const styles = {
+        container: {
+            backgroundColor: darkMode ? '#121212' : '#f4f4f4',
+            color: darkMode ? 'white' : 'black',
+            minHeight: '100vh',
+        }
+    };
+
     return(
         <>
        
         {/*Additional content which changes from page to page*/}
-        <div>
-        <RegisterForm/>
+        <div style={styles.container}>
+            <RegisterForm darkMode={darkMode}/>
         </div> 
         </>
     )
